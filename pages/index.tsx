7b81ca251081f60e31e96935ce540c3755c5acec
Micah R Ledbetter <me@micahrl.com>
2021-01-26 00:04:33 -0600

    I'm making my best efforts here
    And I have decided that my best effort at documentation is linking to a Twitter thread that links to another Twitter thread.
    
    I will not be taking questions at this time

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,21 @@ export default function Home() {
 
       <main className="m-8">
         <h1 className="text-4xl my-8">O.G. IMAGE</h1>
+        <p className="my-8">There is more nothing to see here. Move along.</p>
+        <h2 className="text-2xl my-8">No really, wtf is this?</h2>
         <p className="my-8">
           This generates og:image social preview images for{" "}
           <Link href="https://me.micahrl.com">
             <a className="underline text-blue-600">me</a>
+          </Link>
+          .
+        </p>
+        <p>
+          I wrote{" "}
+          <Link href="https://twitter.com/mrled/status/1353942139772821504">
+            <a className="underline text-blue-600">
+              a thread on Twitter about it
+            </a>
           </Link>
           .
         </p>
@@ -26,7 +37,6 @@ export default function Home() {
           </Link>
           .
         </p>
-        <p className="my-8">There is more nothing to see here. Move along.</p>
       </main>
     </div>
   );
