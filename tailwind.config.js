58ef5e2578b5752eeca0f48c1eef726276fd1b66
Micah R Ledbetter <me@micahrl.com>
2021-01-25 22:34:51 -0600

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["components/**/*.tsx", "lib/**/*.ts", "pages/**/*.tsx", "styles/**/*.css"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
